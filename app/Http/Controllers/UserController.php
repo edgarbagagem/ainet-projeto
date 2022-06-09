69e85cbf0d0904fac455627f6355ba6e58a9ab12
Edgar Bagagem <2002edgargomes@gmail.com>
2022-06-09 10:46:27 +0100

    fix no usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Http\Request;
 use App\Models\User;
 
 class UserController extends Controller
@@ -18,31 +17,29 @@ class UserController extends Controller
         return view('email_index')->with('pageTitle', 'E-Mail');
     }
 
-    public function validatePasswordRequest(Request $request){
+    public function validatePasswordRequest(Request $request)
+    {
+        $user = User::where('email', '=', $request->email)->first();
+        //Check if the user exists
+        if (count($user) < 1) {
+            return redirect()->back()->withErrors(['email' => trans('User does not exist')]);
+        }
 
-        $user = DB::table('users')->where('email', '=', $request->email)
-        ->first();
-    //Check if the user exists
-    if (count($user) < 1) {
-        return redirect()->back()->withErrors(['email' => trans('User does not exist')]);
-    }
-    
-    //Create Password Reset Token
-    DB::table('password_resets')->insert([
-        'email' => $request->email,
-        'token' => str_random(60),
-        'created_at' => Carbon::now()
-    ]);
-    //Get the token just created above
-    $tokenData = DB::table('password_resets')
-        ->where('email', $request->email)->first();
-    
-    if ($this->send_email_with_notification($request->email, $tokenData->token)) {
-        return redirect()->back()->with('status', trans('A reset link has been sent to your email address.'));
-    } else {
-        return redirect()->back()->withErrors(['error' => trans('A Network Error occurred. Please try again.')]);
-    }
+        //Create Password Reset Token
+        DB::table('password_resets')->insert([
+            'email' => $request->email,
+            'token' => str_random(60),
+            'created_at' => Carbon::now()
+        ]);
+        //Get the token just created above
+        $tokenData = DB::table('password_resets')
+            ->where('email', $request->email)->first();
 
+        if ($this->send_email_with_notification($request->email, $tokenData->token)) {
+            return redirect()->back()->with('status', trans('A reset link has been sent to your email address.'));
+        } else {
+            return redirect()->back()->withErrors(['error' => trans('A Network Error occurred. Please try again.')]);
+        }
     }
 
     public function send_email_with_notification($email, $token)
@@ -57,9 +54,8 @@ class UserController extends Controller
             ->with('alert-type', 'success')
             ->with('alert-msg', 'E-Mail sent with success (using Notifications)');`
             */
-            $user = DB::table('users')->where('email', $email)->select('firstname', 'email')->first();
+        $user = User::where('email', $email)->select('firstname', 'email')->first();
 
-            $link = config('base_url') . '/password/reset' . $token . '?email=' . urlencode($user->email);
+        $link = config('base_url') . '/password/reset' . $token . '?email=' . urlencode($user->email);
     }
 }
-
